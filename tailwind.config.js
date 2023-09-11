d8c06b10662990856c7d123aa11292f1a31d71dd
William Cayetano <berkshirereal@gmail.com>
2023-09-11 04:19:56 -0700

    final edit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+
